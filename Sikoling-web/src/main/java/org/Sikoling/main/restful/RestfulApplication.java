5a22cbb9966c242f40a85084093b183fb6af94d6
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-05-21 11:37:11 +0700

    cso

---
+++ Sikoling-web/src/main/java/org/Sikoling/main/restful/RestfulApplication.java
@@ -0,0 +1,9 @@
+package org.Sikoling.main.restful;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestfulApplication extends Application {
+
+}
