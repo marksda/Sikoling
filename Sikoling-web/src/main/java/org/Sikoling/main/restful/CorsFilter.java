3a18b697ad91f0e764a9d9e773bd27100ba1ebf6
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-05-22 22:54:16 +0700

    cso

---
+++ Sikoling-web/src/main/java/org/Sikoling/main/restful/CorsFilter.java
@@ -0,0 +1,31 @@
+package org.Sikoling.main.restful;
+
+import java.io.IOException;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.container.PreMatching;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+@PreMatching
+public class CorsFilter implements ContainerResponseFilter {
+		
+
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+			throws IOException {
+		responseContext.getHeaders().add(
+                "Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+	}
+
+}
