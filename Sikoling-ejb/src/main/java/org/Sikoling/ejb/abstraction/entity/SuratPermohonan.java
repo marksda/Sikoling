6829a3a9ef74958a3d743634d8e31cf434ba720c
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-05-25 14:25:11 +0700

    cso

--- Sikoling-ejb/src/main/java/org/Sikoling/ejb/abstraction/entity/SuratPermohonan.java
+++ Sikoling-ejb/src/main/java/org/Sikoling/ejb/abstraction/entity/SuratPermohonan.java
@@ -2,6 +2,7 @@ package org.Sikoling.ejb.abstraction.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 public class SuratPermohonan implements Serializable {
 
@@ -13,13 +14,18 @@ public class SuratPermohonan implements Serializable {
 	private Alamat alamatKegiatan;
 	private String jenisKegiatan;
 	
-	public SuratPermohonan(Date tanggal, String nomor) {
+	public SuratPermohonan(Date tanggal, String nomor, PenanggungJawab penanggungJawab, Pemrakarsa pemrakarsa,
+			Alamat alamatKegiatan, String jenisKegiatan) {
 		super();
 		this.tanggal = tanggal;
 		this.nomor = nomor;
+		this.penanggungJawab = penanggungJawab;
+		this.pemrakarsa = pemrakarsa;
+		this.alamatKegiatan = alamatKegiatan;
+		this.jenisKegiatan = jenisKegiatan;
 	}
 
-	
+
 	public Date getTanggal() {
 		return tanggal;
 	}
@@ -44,25 +50,115 @@ public class SuratPermohonan implements Serializable {
 		return serialVersionUID;
 	}
 
+	public PenanggungJawab getPenanggungJawab() {
+		return penanggungJawab;
+	}
+	
+
+
+	public void setPenanggungJawab(PenanggungJawab penanggungJawab) {
+		this.penanggungJawab = penanggungJawab;
+	}
+	
+
+
+	public Pemrakarsa getPemrakarsa() {
+		return pemrakarsa;
+	}
+	
+
+
+	public void setPemrakarsa(Pemrakarsa pemrakarsa) {
+		this.pemrakarsa = pemrakarsa;
+	}
+	
+
+
+	public Alamat getAlamatKegiatan() {
+		return alamatKegiatan;
+	}
+	
+
+
+	public void setAlamatKegiatan(Alamat alamatKegiatan) {
+		this.alamatKegiatan = alamatKegiatan;
+	}
+	
+
+
+	public String getJenisKegiatan() {
+		return jenisKegiatan;
+	}
+	
+
+
+	public void setJenisKegiatan(String jenisKegiatan) {
+		this.jenisKegiatan = jenisKegiatan;
+	}
+	
+
 
 	@Override
 	public int hashCode() {
-		// TODO Auto-generated method stub
-		return super.hashCode();
+		int hash = 7;
+		hash = 13 * hash + Objects.hashCode(this.tanggal.toString());
+		hash = 13 * hash + Objects.hashCode(this.nomor);
+		hash = 13 * hash + Objects.hashCode(this.penanggungJawab.toString());
+		hash = 13 * hash + Objects.hashCode(this.pemrakarsa.toString());
+		hash = 13 * hash + Objects.hashCode(this.alamatKegiatan.toString());
+		hash = 13 * hash + Objects.hashCode(this.jenisKegiatan);
+		
+		return hash;
 	}
 
 
 	@Override
 	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
-		return super.equals(obj);
+		if (this == obj) {
+            return true;
+        }
+		
+        if (obj == null) {
+            return false;
+        }
+        
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        final SuratPermohonan other = (SuratPermohonan) obj;
+        
+        if (!Objects.equals(this.tanggal, other.tanggal)) {
+            return false;
+        }
+        
+        if (!Objects.equals(this.nomor, other.nomor)) {
+            return false;
+        }
+        
+        if (!Objects.equals(this.penanggungJawab, other.penanggungJawab)) {
+            return false;
+        }
+        
+        if (!Objects.equals(this.pemrakarsa, other.pemrakarsa)) {
+            return false;
+        }
+        
+        if (!Objects.equals(this.alamatKegiatan, other.alamatKegiatan)) {
+            return false;
+        }
+        
+        if (!Objects.equals(this.jenisKegiatan, other.jenisKegiatan)) {
+            return false;
+        }
+        
+        return true;
 	}
 
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
+		return "SuratPermohonan{" + "tanggal=" + tanggal.toString() + ", nomor=" + nomor + "}";
 	}
 	
 	
--- Sikoling-ejb/src/main/java/org/Sikoling/ejb/abstraction/entity/WaliPemohon.java
+++ Sikoling-ejb/src/main/java/org/Sikoling/ejb/abstraction/entity/WaliPemohon.java
@@ -1,6 +1,7 @@
 package org.Sikoling.ejb.abstraction.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class WaliPemohon implements Serializable {
 
@@ -12,6 +13,65 @@ public class WaliPemohon implements Serializable {
 		super();
 		this.wali = wali;
 		this.status = status;
-	}	
+	}
+
+	public User getWali() {
+		return wali;
+	}
+
+	public void setWali(User wali) {
+		this.wali = wali;
+	}
+
+	public StatusWali getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusWali status) {
+		this.status = status;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 13 * hash + Objects.hashCode(this.wali.getEmail());
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+            return true;
+        }
+		
+        if (obj == null) {
+            return false;
+        }
+        
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        final WaliPemohon other = (WaliPemohon) obj;
+        
+        if (!Objects.equals(this.wali, other.wali)) {
+            return false;
+        }
+        
+        if (!Objects.equals(this.status, other.status)) {
+            return false;
+        }
+        
+        return true;
+	}
+
+	@Override
+	public String toString() {
+		return "WaliPemohon{" + "wali=" + wali.toString() + ", status=" + status.toString() + "}";
+	}
 
 }
