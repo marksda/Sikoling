519448a4b273578db63546861ce6d24808f12071
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-05-30 08:13:04 +0700

    cso

---
+++ Sikoling-ejb/src/main/java/org/Sikoling/ejb/abstraction/service/file/StorageService.java
@@ -0,0 +1,9 @@
+package org.Sikoling.ejb.abstraction.service.file;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface StorageService {
+	String save(String fileName, InputStream inputStream) throws IOException;
+    InputStream load(String fileName) throws IOException;
+}
