51d339cfd129ff6d17e3bcb8b093f3b17fb6a22c
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-05-30 07:28:00 +0700

    cso

--- Sikoling-ejb/src/main/java/org/Sikoling/ejb/abstraction/repository/KelompokBentukUsahaRepository.java
+++ Sikoling-ejb/src/main/java/org/Sikoling/ejb/abstraction/repository/KelompokBentukUsahaRepository.java
@@ -2,7 +2,6 @@ package org.Sikoling.ejb.abstraction.repository;
 
 import java.util.List;
 
-import org.Sikoling.ejb.abstraction.entity.BentukUsaha;
 import org.Sikoling.ejb.abstraction.entity.KelompokBentukUsaha;
 
 public interface KelompokBentukUsahaRepository extends Repository<KelompokBentukUsaha> {
