a3cf651fc4e89434dae90c8f1f1c11b22301493a
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-05-30 09:15:41 +0700

    cso

--- Sikoling-web/src/main/java/org/Sikoling/main/restful/RestfulApplication.java
+++ Sikoling-web/src/main/java/org/Sikoling/main/restful/RestfulApplication.java
@@ -1,9 +1,13 @@
 package org.Sikoling.main.restful;
 
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
 import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("api")
-public class RestfulApplication extends Application {
-
+public class RestfulApplication extends ResourceConfig {
+	public RestfulApplication() {
+		packages("org.Sikoling.main.restful").register(MultiPartFeature.class);
+	}
 }
---
+++ Sikoling-web/src/main/java/org/Sikoling/main/restful/files/FileController.java
@@ -0,0 +1,48 @@
+package org.Sikoling.main.restful.files;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.Sikoling.ejb.abstraction.service.file.StorageService;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import jakarta.ejb.LocalBean;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.UriInfo;
+
+@Stateless
+@LocalBean
+@Path("files")
+public class FileController {
+	@Context
+	private UriInfo uriInfo;
+	@Inject
+	private StorageService storageService;
+	
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.APPLICATION_JSON)
+	public ImageDTO uploadFile(@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail) throws IOException {
+		String fileKey = storageService.save(fileDetail.getFileName(), uploadedInputStream);
+        
+        return new ImageDTO(uriInfo.getBaseUri() + "files/" + fileKey);
+	}
+	
+	@Path("{fileKey}")
+    @GET
+    @Consumes({MediaType.APPLICATION_JSON})
+    public InputStream loadFile(@PathParam("fileKey") String fileKey) throws IOException {
+        return storageService.load(fileKey);
+    }
+}
