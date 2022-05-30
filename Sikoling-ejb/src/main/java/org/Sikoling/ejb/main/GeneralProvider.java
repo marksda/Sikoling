1080a154a5009bbd58aa35215d30f432fe42e57f
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-05-30 08:07:53 +0700

    cso

---
+++ Sikoling-ejb/src/main/java/org/Sikoling/ejb/main/GeneralProvider.java
@@ -0,0 +1,28 @@
+package org.Sikoling.ejb.main;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.Properties;
+
+import jakarta.ejb.LocalBean;
+import jakarta.ejb.Stateless;
+import jakarta.enterprise.inject.Produces;
+
+@Stateless
+@LocalBean
+public class GeneralProvider {
+	
+	@Produces
+	public Properties getProperties() throws IOException {
+		Properties properties = new Properties();
+		
+		InputStream inputStream = GeneralProvider.class.getResourceAsStream(Optional.ofNullable(System.getProperty("ENV"))
+				.map(env -> "/application-" + env + ".properties")
+				.orElse("application.properties"));
+		
+		properties.load(inputStream);
+		
+		return properties;
+	}
+}
---
+++ Sikoling-ejb/src/main/java/org/Sikoling/ejb/main/Infrastructure.java
@@ -0,0 +1,21 @@
+package org.Sikoling.ejb.main;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Documented;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.inject.Qualifier;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({TYPE, METHOD, FIELD, PARAMETER})
+@Documented
+public @interface Infrastructure {
+
+}
