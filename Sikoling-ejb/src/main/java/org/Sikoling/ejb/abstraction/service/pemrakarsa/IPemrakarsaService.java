da5dd1793cf2d6fc2861ae8275d18ea72473c0ed
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-06-13 18:09:58 +0700

    cso

---
+++ Sikoling-ejb/src/main/java/org/Sikoling/ejb/abstraction/service/pemrakarsa/IPemrakarsaService.java
@@ -0,0 +1,18 @@
+package org.Sikoling.ejb.abstraction.service.pemrakarsa;
+
+import java.util.List;
+
+import org.Sikoling.ejb.abstraction.entity.Pemrakarsa;
+
+public interface IPemrakarsaService {
+	Pemrakarsa save(Pemrakarsa t);
+	Pemrakarsa update(Pemrakarsa t);
+	List<Pemrakarsa> getAll();
+	List<Pemrakarsa> getAllByPage(Integer page, Integer pageSize);
+	List<Pemrakarsa> getByQueryNama(String nama);
+	List<Pemrakarsa> getByQueryNamaAndPage(String nama, Integer page, Integer pageSize);
+	List<Pemrakarsa> getByCreator(String idCreator);
+	List<Pemrakarsa> getByCreatorAndPage(String idCreator, Integer page, Integer pageSize);
+	List<Pemrakarsa> getByCreatorAndNama(String idCreator, String nama);
+	List<Pemrakarsa> getByCreatorAndNamaAndPage(String idCreator, String nama, Integer page, Integer pageSize);	
+}
