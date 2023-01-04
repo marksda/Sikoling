d61b1d77e63f38df309ace59f356e172313a27dd
Syaiful Dwi Anwar <vlack123@gmail.com>
2023-01-05 01:01:45 +0700

    cso

--- Sikoling-ejb/src/main/java/org/Sikoling/ejb/main/repository/log/FlowLogData.java
+++ Sikoling-ejb/src/main/java/org/Sikoling/ejb/main/repository/log/FlowLogData.java
@@ -3,6 +3,9 @@ package org.Sikoling.ejb.main.repository.log;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import org.Sikoling.ejb.main.repository.authority.AutorisasiData;
+import org.Sikoling.ejb.main.repository.permohonan.PosisiTahapPemberkasanData;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -25,9 +28,73 @@ public class FlowLogData implements Serializable {
 	
 	private LocalDate tanggal;	
 	
-	@JoinColumn(name="kategori_log", referencedColumnName = "id", insertable = true, updatable = true)
+	@JoinColumn(name = "kategori_log", referencedColumnName = "id", insertable = true, updatable = true)
 	@ManyToOne
 	private KategoriLogData kategoriLogData;
 	
+	@JoinColumn(name = "posisi_tahap_pemberkasan", referencedColumnName = "id", insertable = true, updatable = true)
+	@ManyToOne
+	private PosisiTahapPemberkasanData posisiTahapPemberkasanData;
+	
+	private String keterangan;
+	
+	@JoinColumn(name = "pengakses", referencedColumnName = "id", insertable = true, updatable = true)
+	@ManyToOne
+	private AutorisasiData pengaksesData;
+
+	public FlowLogData() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public LocalDate getTanggal() {
+		return tanggal;
+	}
+
+	public void setTanggal(LocalDate tanggal) {
+		this.tanggal = tanggal;
+	}
+
+	public KategoriLogData getKategoriLogData() {
+		return kategoriLogData;
+	}
+
+	public void setKategoriLogData(KategoriLogData kategoriLogData) {
+		this.kategoriLogData = kategoriLogData;
+	}
+
+	public PosisiTahapPemberkasanData getPosisiTahapPemberkasanData() {
+		return posisiTahapPemberkasanData;
+	}
+
+	public void setPosisiTahapPemberkasanData(PosisiTahapPemberkasanData posisiTahapPemberkasanData) {
+		this.posisiTahapPemberkasanData = posisiTahapPemberkasanData;
+	}
+
+	public String getKeterangan() {
+		return keterangan;
+	}
+
+	public void setKeterangan(String keterangan) {
+		this.keterangan = keterangan;
+	}
+
+	public AutorisasiData getPengaksesData() {
+		return pengaksesData;
+	}
+
+	public void setPengaksesData(AutorisasiData pengaksesData) {
+		this.pengaksesData = pengaksesData;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}	
 	
 }
