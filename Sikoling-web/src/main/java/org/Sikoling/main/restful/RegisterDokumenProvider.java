efdee67c34baeab1394a4eeb0f9448d8fca66a83
Syaiful Dwi Anwar <vlack123@gmail.com>
2023-01-16 09:47:29 +0700

    cso

--- Sikoling-web/src/main/java/org/Sikoling/main/restful/RegisterDokumenProvider.java
+++ Sikoling-web/src/main/java/org/Sikoling/main/restful/RegisterDokumenProvider.java
@@ -17,6 +17,7 @@ import jakarta.json.JsonObject;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import jakarta.json.stream.JsonParser;
+import jakarta.json.stream.JsonParser.Event;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
@@ -38,29 +39,47 @@ public class RegisterDokumenProvider implements MessageBodyReader<RegisterDokume
 			MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
 			throws IOException, WebApplicationException {
 		
-		StringWriter writer = new StringWriter();
-		IOUtils.copy(entityStream, writer, "UTF-8");
-		String theString = writer.toString();
-		
-		JsonParser jsonParser = Json.createParser(new StringReader(theString));
-		JsonObject jsonObject = jsonParser.getObject();
-		String id = jsonObject.getString("id");
-		
+		RegisterDokumenDTO registerDokumenDTO = new RegisterDokumenDTO();
+		DokumenDTO d = null;
 		Jsonb jsonb = JsonbBuilder.create();
 		
-		DokumenDTO d;
+		JsonParser jsonParser = Json.createParser(entityStream);
+		while (jsonParser.hasNext()) {
+		     Event event = jsonParser.next();
+		     if (event == JsonParser.Event.KEY_NAME ) {		    	 
+		         String key = jsonParser.getString();
+		         event = jsonParser.next();
+		         if (key.equals("dokumen")) {
+					 JsonObject jsonDokumenObject = jsonParser.getObject();
+					 String id = jsonDokumenObject.getString("id");
+					 switch (id) {
+					 case "010301":
+						d = jsonb.fromJson(jsonDokumenObject.toString(), NibOssDTO.class);
+						registerDokumenDTO.setDokumen(d); 
+						break;
+					 default:
+						d = null;
+						break;
+					}
+		         }
+		     }
+		 }
 		
-		switch (id) {
-		case "010301":
-			d = jsonb.fromJson(entityStream, NibOssDTO.class);
-			break;
-		default:
-			d = null;
-			break;
-		}
+//		Jsonb jsonb = JsonbBuilder.create();
+//		
+//		DokumenDTO d;
+//		
+//		switch (id) {
+//		case "010301":
+//			d = jsonb.fromJson(entityStream, NibOssDTO.class);
+//			break;
+//		default:
+//			d = null;
+//			break;
+//		}
 		
-		RegisterDokumenDTO registerDokumenDTO = new RegisterDokumenDTO();
-		registerDokumenDTO.setDokumen(d);
+//		RegisterDokumenDTO registerDokumenDTO = new RegisterDokumenDTO();
+//		registerDokumenDTO.setDokumen(d);
 		
 		
 		return registerDokumenDTO;
